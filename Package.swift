d373644a11a7e3c5d1be4a1239147311918569bb
Yehor Popovych <popovych.yegor@gmail.com>
2023-07-26 16:26:27 +0100

    removed non-needed files

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "swift-scale-codec",
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ScaleCodec",
             targets: ["ScaleCodec"]),
@@ -15,8 +15,6 @@ let package = Package(
         .package(url: "https://github.com/tesseract-one/Tuples.swift.git", .branch("main"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ScaleCodec",
             dependencies: [
